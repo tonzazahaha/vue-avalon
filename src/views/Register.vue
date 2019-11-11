1a04bb28075ad3747a0d5b5297b6f367fde2e2e8
eagle <pongpatsitiputa@gmail.com>
2019-11-11 21:53:45 +0700

    regis

---
+++ src/views/Register.vue
@@ -0,0 +1,28 @@
+<template>
+
+  <div>
+    <p>Register page</p>
+    <img alt="Avalon logo" src="../assets/avalon.png">
+    <b-button>Button</b-button>
+    <b-button variant="danger">Button</b-button>
+    <b-button variant="success">Button</b-button>
+    <b-button variant="outline-primary">Button</b-button>
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
