daafc318a5b46fba826f832785c39be6a634b150
F9Uf <atoseka@gmail.com>
2019-11-13 17:54:31 +0700

    Add not show header page method

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <the-header />
+    <the-header v-if="showHeader" />
     <router-view/>
   </div>
 </template>
@@ -12,6 +12,22 @@ export default {
   name: 'app',
   components: {
     TheHeader
+  },
+  data () {
+    return {
+      showHeader: false,
+      pageNotRequiredHeader: ['login', 'register']
+    }
+  },
+  watch: {
+    '$route.name': function (name) {
+      this.showHeader = !this.isInArray(name, this.pageNotRequiredHeader)
+    }
+  },
+  methods: {
+    isInArray (e, array) {
+      return array.indexOf(e) > -1
+    }
   }
 }
 </script>
