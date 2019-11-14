e38ebe98730dd29400ff68ea255438ae986e8afd
eagle <pongpatsitiputa@gmail.com>
2019-11-14 23:01:26 +0700

    login not connected with DB yet

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,37 +1,65 @@
 <template>
-
-  <div>
-    <br>
-    <br>
-    <img alt="Avalon logo" src="../assets/avalon.png" class="mx-auto d-block">
-    <div class = "login">
-     <form>
-         <p style="color:White">Email</p>
-         <input type="text" placeholder="Enter your email" v-model="name"/>
-         <p style="color:White">Password</p>
-         <input type="password" placeholder="Enter your password" v-model="password"/><br>
-         <button type="submit" class="btn btn-primary mb-2">Login</button>
-    </form>
-    <p style="color:White">Don’t have account?<p> <p style="color:Blue" >Create a account</p>
-    </div>
+  <div class="login text-white">
+    <layout-not-auth>
+      <b-form @submit.prevent="onSubmit">
+        <!-- email -->
+        <b-form-group
+          id="email-group"
+          label="Email"
+          label-for="email"
+        >
+          <b-form-input
+            id="email"
+            type="email"
+            v-model="form.email"
+            required
+            placeholder="Enter the email"
+          ></b-form-input>
+        </b-form-group>
+        <!-- password -->
+        <b-form-group
+          id="password-group"
+          label="Password"
+          label-for="password"
+        >
+          <b-form-input
+            id="password"
+            type="password"
+            v-model="form.password"
+            required
+            placeholder="Enter the password"
+          ></b-form-input>
+        </b-form-group>
+        <b-button block class="mt-4" type="submit" variant="primary">Register</b-button>
+        <span class="mt-5">
+          Don't have any account? <router-link to="/register">Create an account</router-link>
+        </span>
+      </b-form>
+    </layout-not-auth>
   </div>
- </template>
+</template>
 
 <script>
-// @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue'
+import LayoutNotAuth from '../components/LayoutNotAuth'
 
 export default {
-  name: 'login'
+  name: 'home',
+  components: {
+    LayoutNotAuth
+  },
+  data () {
+    return {
+      form: {
+        email: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+  }
 }
 </script>
 
 <style scoped>
-.regis{
-  margin: auto;
-  text-align: center;
-  width: 40%;
-  border-radius: 5px;
-  background: rgba(37, 41, 50, 0.7);
-}
+
 </style>
