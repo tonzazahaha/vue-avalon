5e0df00bf81c514a842b08538c9fa187d672c054
F9Uf <atoseka@gmail.com>
2019-11-16 18:01:59 +0700

    reflactor main layout

---
+++ src/components/LayoutMain.vue
@@ -0,0 +1,36 @@
+<template>
+  <b-container fluid>
+    <b-row class="flex-xl-nowrap">
+      <b-col>
+        <v-slot></v-slot>
+      </b-col>
+      <b-col cols="4" class="sidebar bg-secondary px-0 fixed-bottom">
+        <the-sidebar />
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+import TheSidebar from './TheSidebar'
+
+export default {
+  components: {
+    TheSidebar
+  }
+}
+</script>
+
+<style>
+.sidebar {
+  /* display: block; */
+  /* position: sticky; */
+  height: calc(100vh - 50px);
+  top: 60px;
+  left: calc(100vw - 345px);
+  max-width: 345px;
+  min-width: 345px;
+  word-wrap: break-word;
+  z-index: 1000;
+}
+</style>
--- src/views/Lobby.vue
+++ src/views/Lobby.vue
@@ -1,20 +1,13 @@
 <template>
   <div class="lobby">
-    <b-container fluid>
-      <b-row class="flex-xl-nowrap">
-        <b-col>
-          room name
-        </b-col>
-        <b-col cols="4" class="sidebar bg-secondary px-0 fixed-bottom">
-          <the-sidebar />
-        </b-col>
-      </b-row>
-    </b-container>
+    <layout-main>
+      room
+    </layout-main>
   </div>
 </template>
 
 <script>
-import TheSidebar from '../components/TheSidebar'
+import LayoutMain from '../components/LayoutMain'
 
 export default {
   data () {
@@ -22,21 +15,11 @@ export default {
     }
   },
   components: {
-    TheSidebar
+    LayoutMain
   }
 }
 </script>
 
 <style scoped>
-.sidebar {
-  /* display: block; */
-  /* position: sticky; */
-  height: calc(100vh - 50px);
-  top: 60px;
-  left: calc(100vw - 345px);
-  max-width: 345px;
-  min-width: 345px;
-  word-wrap: break-word;
-  z-index: 1000;
-}
+
 </style>
