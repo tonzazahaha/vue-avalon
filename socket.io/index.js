3b5bd58903f29b929463fc13c95e493e372680ff
Ton <monkey33429@gmail.com>
2019-11-16 20:24:42 +0700

    test socket chat

---
+++ socket.io/index.js
@@ -0,0 +1,15 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+http.listen(3000, function(){
+      console.log('listening on *:3000');
+});
+app.get('/', function(req, res){
+      res.sendFile(__dirname+'/index.html');
+});
+io.on('connection',function(client){
+    console.log('Client connected..');
+    client.on('join',function(data){
+        console.log(data);
+    });
+});
