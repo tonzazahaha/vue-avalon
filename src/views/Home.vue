f775271f524780d4fdf884acd229278ce0f06749
eagle <pongpatsitiputa@gmail.com>
2019-11-23 22:33:44 +0700

    home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,7 @@
       <b-row>
         <b-col cols="6" class="my-4 mx-auto">
           <img alt="Avalon logo" class="rounded mx-auto d-block" src="../assets/avalon.png">
+          <button type="button" class="btn btn-outline-secondary btn-block" onclick="window.location='./login/'">Press to enter</button>
         </b-col>
       </b-row>
   </b-container>
