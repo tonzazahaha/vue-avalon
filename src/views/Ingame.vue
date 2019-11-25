be7a270b540c1a8a26dd7d7f153590fe0b281e08
eagle <pongpatsitiputa@gmail.com>
2019-11-25 18:51:58 +0700

    win&lose

--- src/views/Ingame.vue
+++ src/views/Ingame.vue
@@ -13,12 +13,64 @@
         <!-- room footer -->
         <template v-slot:room-footer>
           <b-col cols="auto" class="mx-auto">
-            <b-button variant="success" class="btn-vote mx-3">Approve</b-button>
-            <b-button variant="danger" class="btn-vote mx-3">Reject</b-button>
+            <b-button variant="success" class="btn-vote mx-3" @click="showWin = !showWin">Approve</b-button>
+            <b-button variant="danger" class="btn-vote mx-3"  @click="showLose = !showLose">Reject</b-button>
           </b-col>
         </template>
       </layout-room>
     </layout-main>
+<!-- win -->
+    <b-modal
+      centered
+      ref="my-modal"
+      v-model="showWin"
+      header-bg-variant="secondary"
+      header-border-variant="secondary"
+      body-bg-variant="secondary"
+      footer-bg-variant="secondary"
+      footer-border-variant="secondary"
+      hide-footer title= "Using Component Methods"
+    >
+      <template v-slot:modal-header class="mx-auto">
+        <h5 class="mx-auto">YOU WIN!</h5>
+      </template>
+        <b-row align-h="center">
+          <b-col cols='auto'>
+            <img src="../assets/win.png" alt="win">
+          </b-col>
+        </b-row>
+        <b-row align-h="center">
+          <b-col cols='auto'>
+            <b-button class="mt-3" variant="outline-danger" @click="hideModal">OK</b-button>
+          </b-col>
+        </b-row>
+    </b-modal>
+<!-- lose -->
+    <b-modal
+      centered
+      ref="my-modal"
+      v-model="showLose"
+      header-bg-variant="secondary"
+      header-border-variant="secondary"
+      body-bg-variant="secondary"
+      footer-bg-variant="secondary"
+      footer-border-variant="secondary"
+      hide-footer title= "Using Component Methods"
+    >
+      <template v-slot:modal-header class="mx-auto">
+        <h5 class="mx-auto">YOU LOSE!</h5>
+      </template>
+      <b-row align-h="center">
+          <b-col cols='auto'>
+            <img src="../assets/lose.png" alt="lose">
+          </b-col>
+        </b-row>
+        <b-row align-h="center">
+          <b-col cols='auto'>
+            <b-button class="mt-3" variant="outline-danger" @click="hideModal">OK</b-button>
+          </b-col>
+        </b-row>
+    </b-modal>
   </div>
 </template>
 
@@ -33,6 +85,8 @@ export default {
   },
   data () {
     return {
+      showWin: false,
+      showLose: false,
       game: {
         time: '0'
       }
@@ -41,6 +95,9 @@ export default {
   methods: {
     leaveRoom () {
       this.$router.push('/lobby')
+    },
+    hideModal () {
+      this.$refs['my-modal'].hide()
     }
   }
 }
