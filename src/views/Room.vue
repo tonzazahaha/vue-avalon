45c698e432df3baba27648f7dfde0cc8e227ed8d
neconone <p.chaiolan30@gmail.com>
2019-11-27 03:09:30 +0700

    GameFooter and GameHeader components

--- src/views/Room.vue
+++ src/views/Room.vue
@@ -4,24 +4,26 @@
       <layout-room v-if="room">
         <!-- room detail -->
         <template v-slot:room-detail>
-          <b-col cols="auto" align-self="center" class="ml-0">
-            <span>Room Name</span>
-            <h5>{{ room.roomName }}</h5>
-          </b-col>
-          <b-col cols="auto" align-self="center" class="ml-5">
-            <span>Mode</span>
-            <h5>{{ room.roomMode }}</h5>
-          </b-col>
-          <b-col cols="auto" align-self="center" class="ml-5">
-            <span>Size</span>
-            <h5>{{ room.players.length }}/{{ room.roomSize }}</h5>
-          </b-col>
-          <b-col align-self="center" cols="auto" class="ml-auto">
-            <h5><em class="material-icons pr-2">build</em> Setting</h5>
-          </b-col>
-          <b-col align-self="center" cols="auto" class="ml-autox">
-            <b-button variant="dark" class="px-5 bg-grey" @click="leaveRoom">Leave</b-button>
-          </b-col>
+          <b-row align-v="start" class="px-5 room-detail" style="height: 115px;">
+            <b-col cols="auto" align-self="center" class="ml-0">
+              <span>Room Name</span>
+              <h5>{{ room.roomName }}</h5>
+            </b-col>
+            <b-col cols="auto" align-self="center" class="ml-5">
+              <span>Mode</span>
+              <h5>{{ room.roomMode }}</h5>
+            </b-col>
+            <b-col cols="auto" align-self="center" class="ml-5">
+              <span>Size</span>
+              <h5>{{ room.players.length }}/{{ room.roomSize }}</h5>
+            </b-col>
+            <b-col align-self="center" cols="auto" class="ml-auto">
+              <h5><em class="material-icons pr-2">build</em> Setting</h5>
+            </b-col>
+            <b-col align-self="center" cols="auto" class="ml-autox">
+              <b-button variant="dark" class="px-5 bg-grey" @click="leaveRoom">Leave</b-button>
+            </b-col>
+          </b-row>
         </template>
         <!-- room wrapper -->
         <template v-slot:room-wrapper>
