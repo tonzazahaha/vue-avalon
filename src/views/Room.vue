cbe01fd1631a452cd135461094fd11cbe2df756c
neconone <p.chaiolan30@gmail.com>
2019-11-30 21:45:48 +0700

    Room

--- src/views/Room.vue
+++ src/views/Room.vue
@@ -45,43 +45,18 @@
       v-if="room.gamePhase === 6 && playerWin"
     >
       <template v-slot:modal-header class="mx-auto">
-        <h5 class="mx-auto">YOU WIN!</h5>
+        <h5 v-if="playerWin === true" class="mx-auto">YOU WIN!</h5>
+        <h5 v-else class="mx-auto">YOU LOSE!</h5>
       </template>
         <b-row align-h="center">
           <b-col cols='auto'>
-            <img src="../assets/win.png" alt="win">
+            <img v-if="playerWin" src="../assets/win.png" alt="win">
+            <img v-if="playerWin" src="../assets/lose.png" alt="lose">
           </b-col>
         </b-row>
         <b-row align-h="center">
           <b-col cols='auto'>
             <b-button class="mt-3" variant="outline-primary" @click="hideModal">OK</b-button>
-          </b-col>
-        </b-row>
-    </b-modal>
-    <!-- lose -->
-    <b-modal
-      centered
-      ref="my-modal1"
-      v-model="showLose"
-      header-bg-variant="secondary"
-      header-border-variant="secondary"
-      body-bg-variant="secondary"
-      footer-bg-variant="secondary"
-      footer-border-variant="secondary"
-      hide-footer title= "Using Component Methods"
-      v-if="room.gamePhase === 6 && !playerWin"
-    >
-      <template v-slot:modal-header class="mx-auto">
-        <h5 class="mx-auto">YOU LOSE!</h5>
-      </template>
-      <b-row align-h="center">
-          <b-col cols='auto'>
-            <img src="../assets/lose.png" alt="lose">
-          </b-col>
-        </b-row>
-        <b-row align-h="center">
-          <b-col cols='auto'>
-            <b-button class="mt-3" variant="outline-primary" @click="hideModal1">OK</b-button>
           </b-col>
         </b-row>
     </b-modal>
